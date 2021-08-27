173d8814f09f2da5d1872cc6e38e2908ca1aa2d9
lcolladotor <lcollado@jhsph.edu>
2021-08-27 09:10:22 -0400

    Adapt cellranger code from /dcl01/ajaffe/data/lab/singleCell/10x_pilot/premRNA/Feb2021/jobSubmit_Br2743_DLPFC-mid_CR3.0-premRNA.sh and udpate from 3.0.2 to 6.1.1. Also update the array job pieces

--- code/01_align/round1.sh
+++ code/01_align/round1.sh
@@ -26,7 +26,25 @@ module load cellranger/6.1.1
 module list
 
 ## Locate file
-FILE1=$(awk 'BEGIN {FS="\t"} {print \$1}' ${JOB_ID}.txt | awk "NR==${SGE_TASK_ID}")
+SAMPLE=$(awk 'BEGIN {FS="\t"} {print \$1}' ${JOB_ID}.txt | awk "NR==${SGE_TASK_ID}")
+echo "Processing sample ${SAMPLE}"
+date
+
+## Run CellRanger
+cellranger count --id=${SAMPLE} \
+    --transcriptome=/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A \
+    --fastqs=/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/raw-data/FASTQ/${SAMPLE}_L001,/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/raw-data/FASTQ/${SAMPLE}_L002 \
+    --sample=${SAMPLE} \
+    --jobmode=local \
+    --localcores=4 \
+    --localmem=140 \
+    --include-introns
+
+## Move output
+echo "Moving data to new location"
+date
+mkdir -p /dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/cellranger/
+mv ${SAMPLE} /dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/cellranger/
 
 echo "**** Job ends ****"
 date
