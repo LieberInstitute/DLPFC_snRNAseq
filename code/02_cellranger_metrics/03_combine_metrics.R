7f884a22ff61795855b34a924c160d6fc27d8776
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-08-31 15:36:51 -0400

    Paste code from https://github.com/LieberInstitute/Visium_IF_AD/blob/master/code/analysis/02_sample_metrics.R#L196-L208

--- code/02_cellranger_metrics/03_combine_metrics.R
+++ code/02_cellranger_metrics/03_combine_metrics.R
@@ -24,6 +24,38 @@ load(
 all_metrics <- merge(tran_metrics, cellranger_metrics, all = TRUE)
 dim(all_metrics)
 
+## Compare metrics one at a time across studies
+## Used https://rpkgs.datanovia.com/ggpubr/reference/ggboxplot.html
+pdf(
+    here(
+        "processed-data",
+        "02_cellranger_metrics",
+        "cross_study_cellranger_metrics_boxplots.pdf"
+    ),
+    useDingbats = FALSE,
+    width = 10
+)
+for (i in colnames(all_metrics)[-c(1, ncol(all_metrics))]) {
+    set.seed(20210714)
+    p <-
+        ggboxplot(
+            all_metrics,
+            x = "study",
+            y = i,
+            color = "study",
+            palette = "Dark2",
+            add = "jitter",
+            shape = "study",
+            label = "Sample.ID",
+            repel = TRUE,
+            font.label = list(size = 5),
+            legend = "none",
+            ggtheme = theme_pubr(base_size = 30)
+        )
+    print(p)
+}
+dev.off()
+
 
 
 ## Reproducibility information
