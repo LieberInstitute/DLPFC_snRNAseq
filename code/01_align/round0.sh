dc919a07d07a1eae854f10afaacd66a50d6c59f5
Matt N Tran <nnguye44@jhmi.edu>
2021-11-23 15:07:54 -0500

    Run pilot sample ('round0') through CR6.1

--- code/01_align/round0.sh
+++ code/01_align/round0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l bluejay,mem_free=35G,h_vmem=35G,h_fsize=100G
+#$ -l bluejay,mem_free=5G,h_vmem=5G,h_fsize=100G
 #$ -pe local 4
 #$ -N round0
 #$ -o logs/round0.txt
@@ -15,16 +15,35 @@ echo "User: ${USER}"
 echo "Job id: ${JOB_ID}"
 echo "Job name: ${JOB_NAME}"
 echo "Hostname: ${HOSTNAME}"
-echo "Task id: ${SGE_TASK_ID}"
 
-## Load the R module (absent since the JHPCE upgrade to CentOS v7)
-module load conda_R
 
 ## List current modules for reproducibility
 module list
 
-## Edit with your job command
+## Load Cell Ranger
 module load cellranger/6.1.1
+
+## Locate file
+SAMPLE=$(awk "NR==1" ${JOB_NAME}.txt)
+echo "Processing sample ${SAMPLE}"
+date
+
+## Run CellRanger
+cellranger count --id=${SAMPLE} \
+    --transcriptome=/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x/refdata-gex-GRCh38-2020-A \
+    --fastqs=/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/raw-data/FASTQ/${SAMPLE} \
+    --sample=Br2743-DLPFC-mid \
+    --jobmode=local \
+    --localcores=4 \
+    --localmem=20 \
+    --include-introns
+
+## Move output
+echo "Moving data to new location"
+date
+#mkdir -p /dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/cellranger/
+mv ${SAMPLE} /dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/cellranger/
+
 
 echo "**** Job ends ****"
 date
