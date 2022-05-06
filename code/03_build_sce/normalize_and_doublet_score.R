81ce5b36bbc747676a62c290c1271ce98bc29537
lahuuki <lahuuki@gmail.com>
2022-05-06 15:11:16 -0400

    Improve QC plots, add doublet scores #4

--- code/03_build_sce/normalize_and_doublet_score.R
+++ code/03_build_sce/normalize_and_doublet_score.R
@@ -2,10 +2,16 @@ library("SingleCellExperiment")
 library("scran")
 library("scater")
 library("scuttle")
+library("scDblFinder")
+library("jaffelab")
 library("tidyverse")
 library("batchelor")
 library("here")
 library("sessioninfo")
+
+# Plotting themes
+my_theme <- theme_bw() +
+  theme(text = element_text(size=15))
 
 ## Load empty-free sce data
 load(here("processed-data", "sce", "sce_no_empty_droplets.Rdata"), verbose = TRUE)
@@ -51,30 +57,6 @@ table(sce$low_sum, sce$low_detected)
 #       FALSE  TRUE
 # FALSE 83513   781
 # TRUE      0   462
-
-#### QC plots ####
-pdf(here("plots","03_build_sce","QC_outliers.pdf"), width = 21)
-## Mito rate
-plotColData(sce, x = "Sample", y="subsets_Mito_percent", colour_by="high_mito") +
-  ggtitle("Mito Precent")+
-  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
-
-# ## low sum
-plotColData(sce, x = "Sample", y="sum", colour_by="low_sum") +
-  scale_y_log10() +
-  ggtitle("Total count") +
-  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
-# +
-#   geom_hline(yintercept = 1000) ## hline doesn't work w/ facet_wrap?
-
-# ## low detected
-plotColData(sce, x = "Sample", y="detected", colour_by="low_detected") +
-  scale_y_log10() +
-  ggtitle("Detected features") +
-  # geom_hline(yintercept = 500)+
-  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
-
-dev.off()
 
 ## Annotate nuc to drop
 sce$discard_auto <- sce$high_mito | sce$low_sum | sce$low_detected
@@ -133,7 +115,109 @@ round(100 * sweep(qc_t, 1, qc_t[, 3], "/"), 1)
 # Br8667_mid   99.7   0.3 100.0
 # Sum          91.6   8.4 100.0
 
-## To speed up, run on sample-level top-HVGs - just take top 1000 ===
-pilot.data.normd <- lapply(pilot.data.2, logNormCounts)
-geneVar.samples <- lapply(pilot.data.normd, modelGeneVar)
-topHVGs <- lapply(geneVar.samples, function(x) {getTopHVGs(x, n=1000)})
+#### QC plots ####
+pdf(here("plots","03_build_sce","QC_outliers.pdf"), width = 21)
+## Mito rate
+plotColData(sce, x = "Sample", y="subsets_Mito_percent", colour_by="high_mito") +
+  ggtitle("Mito Precent")+
+  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
+
+# ## low sum
+plotColData(sce, x = "Sample", y="sum", colour_by="low_sum") +
+  scale_y_log10() +
+  ggtitle("Total count") +
+  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
+# +
+#   geom_hline(yintercept = 1000) ## hline doesn't work w/ facet_wrap?
+
+# ## low detected
+plotColData(sce, x = "Sample", y="detected", colour_by="low_detected") +
+  scale_y_log10() +
+  ggtitle("Detected features") +
+  # geom_hline(yintercept = 500)+
+  facet_wrap(~sce$round, scales = "free_x", nrow = 1)
+
+# Mito rate vs n detected features
+
+  plotColData(sce, x="detected", y="subsets_Mito_percent",
+              colour_by="discard_auto", point_size=2.5, point_alpha=0.5) 
+
+# Detected features vs total count
+
+  plotColData(sce, x="sum", y="detected",
+              colour_by="discard_auto", point_size=2.5, point_alpha=0.5) 
+
+dev.off()
+
+
+#### Doublet detection / removal ####
+## To speed up, run on sample-level top-HVGs - just take top 1000 
+set.seed(506)
+
+dbl_dens_sample <- map(splitit(sce$Sample), function(s){
+  sce_temp <- sce[,s]
+  ## To speed up, run on sample-level top-HVGs - just take top 1000 
+  normd <- logNormCounts(sce_temp)
+  geneVar <- modelGeneVar(normd)
+  topHVGs <- getTopHVGs(geneVar, n=1000)
+  
+  dbl_dens <- computeDoubletDensity(normd, subset.row=topHVGs)
+  return(dbl_dens)
+})
+
+map_dbl(dbl_dens_sample, median)
+# Br2720_mid Br2720_post  Br2743_ant  Br2743_mid  Br3942_ant  Br3942_mid  Br6423_ant Br6423_post  Br6432_ant  Br6471_ant 
+# 0.314412    1.047200    0.377364    0.267228    0.376140    0.448136    0.351912    0.300958    0.480480    0.347712 
+# Br6471_mid  Br6522_mid Br6522_post  Br8325_ant  Br8325_mid  Br8492_mid Br8492_post  Br8667_ant  Br8667_mid 
+# 0.278304    1.654350    1.812490    0.297550    0.249500    0.456632    0.514108    0.429644    0.471276 
+
+map(dbl_dens_sample, ~round(quantile(.x, probs=seq(0,1,by=0.05)),1))
+
+## Visualize doublet scores ##
+
+dbl_df <- stack(dbl_dens_sample) %>%
+  rename(dbl_score = values, Sample = ind)
+
+dbl_box_plot <- dbl_df %>%
+  ggplot(aes(x = reorder(Sample, dbl_score, FUN = median), y = dbl_score)) +
+  geom_boxplot() +
+  labs(x = "Sample") +
+  geom_hline(yintercept = 5, color = "red", linetype = "dashed") +
+  coord_flip() +
+  my_theme
+
+ggsave(dbl_box_plot, filename = here("plots","03_build_sce","doublet_scores_boxplot.png"))
+
+dbl_density_plot <- dbl_df %>%
+  ggplot(aes(x  = dbl_score)) +
+  geom_density() +
+  labs(x = "doublet score") +
+  facet_grid(Sample ~ .) +
+  theme_bw()
+
+ggsave(dbl_density_plot, filename = here("plots","03_build_sce","doublet_scores_desnity.png"), height = 17)
+
+
+# Percent that would be dropped at density score >= 5
+map_int(dbl_dens_sample,  ~sum(.x >= 5))
+# Br2720_mid Br2720_post  Br2743_ant  Br2743_mid  Br3942_ant  Br3942_mid  Br6423_ant Br6423_post  Br6432_ant  Br6471_ant 
+# 45          97          21          11          52           5          30          23          32          37 
+# Br6471_mid  Br6522_mid Br6522_post  Br8325_ant  Br8325_mid  Br8492_mid Br8492_post  Br8667_ant  Br8667_mid 
+# 49         135         366          60          31          10           4          39          12 
+map_dbl(dbl_dens_sample,  ~round(100*sum(.x >= 5)/length(.x), 1))
+# Br2720_mid Br2720_post  Br2743_ant  Br2743_mid  Br3942_ant  Br3942_mid  Br6423_ant Br6423_post  Br6432_ant  Br6471_ant 
+# 1.2         1.6         0.7         0.3         0.8         0.1         0.8         0.6         0.9         1.0 
+# Br6471_mid  Br6522_mid Br6522_post  Br8325_ant  Br8325_mid  Br8492_mid Br8492_post  Br8667_ant  Br8667_mid 
+# 0.9         3.3         8.5         1.0         0.6         0.2         0.1         0.7         0.3 
+
+
+cn_test <- map(splitit(sce$Sample), function(s){
+  sce_temp <- sce[,s]
+  return(colnames(sce_temp))})
+
+cn_test2 <- unlist(cn_test)[unlist(splitit(sce$Sample))]
+
+table(cn_test2 == colnames(sce))
+table(head(unlist(cn_test)) == head(colnames(sce)))
+all(unlist(cn_test) == colnames(sce))
+
