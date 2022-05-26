9ae40c77f2026250a83b6401aa3703977b67f7e6
lahuuki <lahuuki@gmail.com>
2022-05-26 12:16:28 -0400

    User troubleshoot script to re-run problem samples with user defined cutoff, run on Br6522 post + mid

---
+++ code/03_build_sce/get_droplet_scores_troubleshoot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -N get_droplet_scores_troubleshoot
+#$ -o logs/get_droplet_scores_troubleshoot.txt
+#$ -e logs/get_droplet_scores_troubleshoot.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript get_droplet_scores_troubleshoot.R Br6522_mid 300
+Rscript get_droplet_scores_troubleshoot.R Br6522_post 300
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
