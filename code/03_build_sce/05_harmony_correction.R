a1faa89d5870b0e02d09f90f0c3d01cbe0cd37d8
lahuuki <lahuuki@gmail.com>
2022-06-10 11:23:36 -0400

    Use GLMPCA to run mharmony by Sample #5

--- code/03_build_sce/05_harmony_correction.R
+++ code/03_build_sce/05_harmony_correction.R
@@ -10,12 +10,17 @@ args = commandArgs(trailingOnly=TRUE)
 correction = args[1]
 
 ## Load empty-free sce data
-load(here("processed-data", "03_build_sce","sce_uncorrected.Rdata"), verbose = TRUE)
+load(here("processed-data", "03_build_sce","sce_uncorrected_glm.Rdata"), verbose = TRUE)
 stopifnot(correction %in% colnames(colData(sce_uncorrected)))
 
 ## Run harmony
+## needs PCA & GLMPCA? not sure which it acctauly uses? 
+reducedDim(sce_uncorrected, "PCA") <- reducedDim(sce_uncorrected, "GLMPCA_approx")
+# Error in RunHarmony.SingleCellExperiment(sce_uncorrected, reduction = "GLMPCA_approx",  : 
+#                                            PCA must be computed before running Harmony.
+
 message("running Harmony - ", Sys.time())
-sce <- RunHarmony(sce_uncorrected, correction, verbose = TRUE)
+sce <- RunHarmony(sce_uncorrected, reduction="GLMPCA_approx", group.by.vars = correction, verbose = TRUE)
 
 set.seed(602)
 
@@ -29,10 +34,8 @@ message("Done TSNE + UMAP - Saving data...", Sys.time())
 
 save(sce, file = here("processed-data", "03_build_sce",paste0("sce_harmony_",correction,".Rdata")))
 
-
-# sgejobs::job_single('normalize_harmony_round', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript normalize_harmony.R round")
-# sgejobs::job_single('normalize_harmony_subject', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript normalize_harmony.R subject")
-# sgejobs::job_single('normalize_harmony_Sample', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript normalize_harmony.R Sample")
+## going with Sample
+# sgejobs::job_single('05_harmony_correction_Sample', create_shell = TRUE, queue= 'bluejay', memory = '25G', command = "Rscript 05_harmony_correction.R Sample")
 
 ## Reproducibility information
 print('Reproducibility information:')
