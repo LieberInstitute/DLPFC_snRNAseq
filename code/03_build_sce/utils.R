69b97e435f0c2bccd9c313bc0c654e7faf478697
lahuuki <lahuuki@gmail.com>
2022-06-13 16:49:40 -0400

    Add plotExpressionCustom from Tran, Maynard scripts to utils

--- code/03_build_sce/utils.R
+++ code/03_build_sce/utils.R
@@ -46,3 +46,28 @@ plot_reducedDim_qc <- function(sce, type = "TSNE", color_by = "sum", title =""){
   return(main_plot + labs(title = title))  
   
 }
+
+
+plotExpressionCustom <- function(sce, features, features_name, anno_name = "cellType",
+                                 point_alpha=0.2, point_size=0.7, ncol=2, xlab = NULL,
+                                 exprs_values = "logcounts", scales = "fixed"){
+  scater::plotExpression(sce, 
+                         exprs_values = exprs_values, 
+                         features = features,
+                         x = anno_name, 
+                         colour_by = anno_name,
+                         ncol = ncol,
+                         xlab = xlab,
+                         point_alpha = point_alpha, 
+                         point_size = point_size,
+                         add_legend = F,
+                         scales = scales) +
+    stat_summary(fun = median, 
+                 fun.min = median, 
+                 fun.max = median,
+                 geom = "crossbar", 
+                 width = 0.3) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          strip.text = element_text(face = "italic")) +  
+    ggtitle(label=paste0(features_name, " markers"))
+}
