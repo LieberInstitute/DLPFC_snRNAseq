a6bcf6550aa842febc151c882bb24f6ff10934bb
lahuuki <lahuuki@gmail.com>
2022-06-21 13:04:18 -0400

    Add my_plotMarkeres to loop throught marker list and make pdf

--- code/03_build_sce/my_plotExpression.R
+++ code/03_build_sce/my_plotExpression.R
@@ -46,3 +46,28 @@ my_plotExpression <- function(sce, genes, assay = "logcounts", cat = "cellType",
   
 }
 
+
+my_plotMarkers <- function(sce, marker_list, assay = "logcounts", cat = "cellType", fill_colors = NULL, pdf_fn){
+  message("plotting: ", pdf_fn)
+  pdf(pdf_fn, height=6, width=8)
+  
+  for(i in 1:length(marker_list)){
+    message(names(marker_list)[[i]])
+    
+    m <- marker_list[[i]]
+    markers <- m[m %in% rownames(sce)]
+    
+    # if(m != markers) message("Missing...",paste(m[!m %in% markers], collapse = ", "))
+    if(m != markers) message("Missing markers...")
+    print(
+      my_plotExpression(sce,
+                        genes = markers, 
+                        title = names(marker_list)[[i]],
+                        cat = cat,
+                        fill_colors = fill_colors)
+    )
+  }
+  
+  dev.off()
+}
+
