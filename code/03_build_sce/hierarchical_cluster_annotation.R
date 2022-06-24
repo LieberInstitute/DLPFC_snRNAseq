097818a81092443fa8544d44770950ec5c3c9adb
lahuuki <lahuuki@gmail.com>
2022-06-24 15:40:53 -0400

    Plot cluster expression with new Mathys/Tran marker list, replot with cell type annotations #6

--- code/03_build_sce/hierarchical_cluster_annotation.R
+++ code/03_build_sce/hierarchical_cluster_annotation.R
@@ -6,6 +6,7 @@ library("dynamicTreeCut")
 library("here")
 library("sessioninfo")
 library("dplyr")
+library("numform")
 
 # Plotting set up 
 my_theme <- theme_bw() +
@@ -86,30 +87,30 @@ table(sce$prelimCluster)[names(doublet_clust)]
 #           ** That is, to pseudo-bulk (aka 'cluster-bulk') on raw counts, on all [non-zero] genes,
 #              normalize with `librarySizeFactors()`, log2-transform, then perform HC'ing
 
-
-prelimCluster.PBcounts <- aggregateAcrossCells(sce,  DataFrame(
-  prelimCluster = sce$prelimCluster
-))
-
-table(rowSums(assays(prelimCluster.PBcounts)$counts) == 0)
-# # FALSE  TRUE
-# # 34987  1614
-#
-# # Compute LSFs at this level
-sizeFactors.PB.all  <- librarySizeFactors(assays(prelimCluster.PBcounts)$counts)
-#
-# # Normalize with these LSFs
-geneExprs.temp <- t(apply(assays(prelimCluster.PBcounts)$counts, 1, function(x) {log2(x/sizeFactors.PB.all + 1)}))
-
-## Perform hierarchical clustering
-dist.clusCollapsed <- dist(t(geneExprs.temp))
-tree.clusCollapsed <- hclust(dist.clusCollapsed, "ward.D2")
-
-dend <- as.dendrogram(tree.clusCollapsed, hang=0.2)
-
-## save HC data
-save(dend, tree.clusCollapsed, dist.clusCollapsed, file = here("processed-data", "03_build_sce", "HC_dend.Rdata"))
-# load(here("processed-data", "03_build_sce", "HC_dend.Rdata"), verbose = TRUE)
+# 
+# prelimCluster.PBcounts <- aggregateAcrossCells(sce,  DataFrame(
+#   prelimCluster = sce$prelimCluster
+# ))
+# 
+# table(rowSums(assays(prelimCluster.PBcounts)$counts) == 0)
+# # # FALSE  TRUE
+# # # 34987  1614
+# #
+# # # Compute LSFs at this level
+# sizeFactors.PB.all  <- librarySizeFactors(assays(prelimCluster.PBcounts)$counts)
+# #
+# # # Normalize with these LSFs
+# geneExprs.temp <- t(apply(assays(prelimCluster.PBcounts)$counts, 1, function(x) {log2(x/sizeFactors.PB.all + 1)}))
+# 
+# ## Perform hierarchical clustering
+# dist.clusCollapsed <- dist(t(geneExprs.temp))
+# tree.clusCollapsed <- hclust(dist.clusCollapsed, "ward.D2")
+# 
+# dend <- as.dendrogram(tree.clusCollapsed, hang=0.2)
+# 
+# ## save HC data
+# save(dend, tree.clusCollapsed, dist.clusCollapsed, file = here("processed-data", "03_build_sce", "HC_dend.Rdata"))
+load(here("processed-data", "03_build_sce", "HC_dend.Rdata"), verbose = TRUE)
 
 ## pick k clusters?
 # clus = cutree(dend, 19)
@@ -190,7 +191,6 @@ if(any(clust.treeCut[order.dendrogram(dend)] == 0)){
 names(clust.treeCut) <- paste0("HC", numform::f_pad_zero(names(clust.treeCut)))
 cluster_colors <- DeconvoBuddies::create_cell_colors(cell_types = sort(unique(names(clust.treeCut))), pallet = "gg")
 
-
 labels_colors(dend) <- cluster_colors[clust.treeCut[order.dendrogram(dend)]]
 
 # Print for future reference
@@ -238,31 +238,41 @@ ggsave(TSNE_clusters +
        filename = here(plot_dir, "clusters_HC-29.png"), width = 10)
 
 
+# Just for logcounts
+sce <- batchelor::multiBatchNorm(sce, batch=sce$Sample)
+rownames(sce) <- rowData(sce)$gene_name
+# load(here("processed-data", "sce","sce_DLPFC.Rdata"), verbose = TRUE)
+
 #### compare w/ mb k-means ####
 load(here("processed-data", "03_build_sce","km_res.Rdata"), verbose = TRUE)
 k_list <- seq(5, 50) ## keep index
 sce$kmeans<- as.factor(paste0("mbk", numform::f_pad_zero(km_res[[which(k_list==29)]]$Clusters)))
 table(sce$kmeans)
 
+load(here("processed-data", "03_build_sce","color_palletes.Rdata"), verbose = TRUE)
+names(iWantHue_k29) <- levels(sce$kmeans)
+
 #### Marker Genes ####
-# Just for logcounts
-sce <- batchelor::multiBatchNorm(sce, batch=sce$Sample)
-
 # load Mathy's markers
-load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/revision/markers.rda", verbose = TRUE)
-# markers.mathys.custom
-all(unlist(markers.mathys.custom) %in% rowData(sce)$gene_name)
-
-rownames(sce) <- rowData(sce)$gene_name
+load(here("processed-data", "03_build_sce", "markers.mathys.tran.Rdata"), verbose = TRUE)
+# markers.mathys.tran
+all(unlist(markers.mathys.tran) %in% rowData(sce)$gene_name)
+# [1] TRUE
 
 source("my_plotExpression.R")
 
 ## Mathys
 my_plotMarkers(sce = sce, 
-               marker_list = markers.mathys.custom,
+               marker_list = markers.mathys.tran,
                cat = "collapsedCluster",
                fill_colors = cluster_colors,
                pdf_fn = here(plot_dir, "HC_mathys_markers.pdf"))
+
+my_plotMarkers(sce = sce, 
+               marker_list = markers.mathys.tran,
+               cat = "kmeans",
+               fill_colors = iWantHue_k29,
+               pdf_fn = here(plot_dir, "mb_kmeans_29_Tran_markers.pdf"))
 
 ## Tran 
 dlpfc_markers <- read.csv("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/tables/revision/top40genesLists_DLPFC-n3_cellType_SN-LEVEL-tests_LAH2020.csv")
@@ -292,15 +302,6 @@ my_plotMarkers(sce = sce,
                fill_colors = cluster_colors,
                pdf_fn = here(plot_dir, "HC_MeanRatio_markers.pdf"))
 
-my_plotClusterMarkers(sce = sce,
-                      marker_list = markers.mathys.custom,
-                      cat = "collapsedCluster",
-                      pdf_fn = here(plot_dir, "pcm_test2.pdf"))
-
-my_plotClusterMarkers(sce = sce,
-                      marker_list = markers.mathys.custom,
-                      cat = "kmeans",
-                      pdf_fn = here(plot_dir, "pcm_test2.pdf"))
 
 my_plotClusterMarkers(sce = sce,
                       marker_list = mr_list,
@@ -308,49 +309,122 @@ my_plotClusterMarkers(sce = sce,
                       pdf_fn = here(plot_dir, "pcm_MR.pdf"))
 
 pdf(here(plot_dir, "pcm_test.pdf"), height=6, width=8)
-my_plotExpression_flip(sce[,sce$kmeans == "mbk04"],
-               marker_list = markers.mathys.custom, 
+my_plotExpression_flip(sce[,sce$kmeans == "mbk05"],
+               marker_list = markers.mathys.tran, 
                assay = "logcounts")
 dev.off()
 
 ## check out annotation 
 anno_k <- read.csv(here("processed-data", "03_build_sce", "DLPFC_k29_anno.csv"))
-
-anno_k2 <- anno_k %>% 
-  arrange(broad) %>%
-  group_by(broad) %>% 
-  mutate(ci = as.character(row_number()),
-         ct = paste0(broad,'_',ci),
-         cellType = ifelse(n()>1,"multi",broad))
-
-anno_k2 %>% select(broad,ci,ct,cellType)
-
 table(anno_k$broad)
+
+## Add index to multiple cell types
+anno_k2 <- anno_k %>% 
+  group_by(broad) %>% 
+  mutate(ct = paste0(broad,'_',f_pad_zero(row_number(), width = 2)),
+         n = n()) %>%
+  ungroup() %>%
+  mutate(cellType = as.factor(ifelse(n>1,ct,broad)))
+
+anno_k2 %>% select(broad,ct,cellType) %>%arrange(cellType)
+anno_k2 %>% filter(n == 1)
+
 
 anno_hc <- read.csv(here("processed-data", "03_build_sce", "DLPFC_HC_anno.csv"))
 table(anno_hc$broad)
 
-sce$cellType_k <- factor(anno_k$broad[match(sce$kmeans, anno_k$cluster)])
-table(sce$cellType_k)
+anno_hc2 <- anno_hc %>% 
+  group_by(broad) %>% 
+  mutate(ct = paste0(broad,'_',f_pad_zero(row_number(), width = 2)),
+         n = n()) %>%
+  ungroup() %>%
+  mutate(cellType = as.factor(ifelse(n>1,ct,broad)))
+
+
+## Assign to sce
+sce$cellType_broad_k <- factor(anno_k$broad[match(sce$kmeans, anno_k$cluster)])
+sce$cellType_k <- factor(anno_k2$cellType[match(sce$kmeans, anno_k2$cluster)])
+
+table(sce$cellType_broad_k)
 # Ambig Astro Excit Inhib Micro Mural  Neun Oligo   OPC 
 # 2245  3557 18042  8173  5552  1330  3198 33716  1791
 
-(prop_k <- 100*round(table(sce$cellType_k)/ncol(sce),3))
-# Ambig Astro Excit Inhib Micro Mural  Neun Oligo   OPC 
-# 2.9   4.6  23.2  10.5   7.2   1.7   4.1  43.4   2.3 
+table(sce$cellType_k)
+# Ambig_1  Ambig_2    Astro     Endo  Excit_1 Excit_10 Excit_11 Excit_12 Excit_13  Excit_2  Excit_3  Excit_4  Excit_5 
+# 3     2242     3557     1330     5879     1732     1292      295        7     1058      548     1188       82 
+# Excit_6  Excit_7  Excit_8  Excit_9  Inhib_1  Inhib_2  Inhib_3  Inhib_4  Inhib_5  Inhib_6  Micro_1  Micro_2     Neun 
+# 633     2851      896     1581      132      461        2     6018     1311      249       11     5541     3198 
+# Oligo_1  Oligo_2      OPC 
+# 28085     5631     1791
 
-sce$cellType_hc <- factor(anno_hc$broad[match(sce$collapsedCluster, anno_hc$cluster)])
+
+sce$cellType_broad_hc <- factor(anno_hc$broad[match(sce$collapsedCluster, anno_hc$cluster)])
+sce$cellType_hc <- factor(anno_hc2$cellType[match(sce$collapsedCluster, anno_hc2$cluster)])
+table(sce$cellType_broad_hc)
 table(sce$cellType_hc)
 # Ambig Astro  Endo Excit Inhib Micro Mural  Neun Oligo   OPC 
 # 1310  3979   446 21766  9757  1601  1711  3043 32051  1940 
 
-(prop_hc <- 100*round(table(sce$cellType_hc)/ncol(sce),3))
+## Check out prop
+(prop_k <- 100*round(table(sce$cellType_broad_k)/ncol(sce),3))
+# Ambig Astro Excit Inhib Micro Mural  Neun Oligo   OPC 
+# 2.9   4.6  23.2  10.5   7.2   1.7   4.1  43.4   2.3 
+
+(prop_hc <- 100*round(table(sce$cellType_broad_hc)/ncol(sce),3))
 # Ambig Astro  Endo Excit Inhib Micro Mural  Neun Oligo   OPC 
 # 1.7   5.1   0.6  28.0  12.6   2.1   2.2   3.9  41.3   2.5 
 
 ## Tram, Maynard DLPFC dataset
 # Astro      Excit      Inhib Macrophage      Micro      Mural      Oligo        OPC      Tcell 
 # 7.0       21.3       14.1        0.1        3.5        0.2       48.7        5.1        0.1 
+
+
+#### Replot with Annotations ####
+ct <- sort(unique(as.character(c(anno_hc2$cellType,anno_k2$cellType))))
+ct2 <- ct[!grepl("Ambig", ct)]
+p1 <- DeconvoBuddies::create_cell_colors(ct2, split = "_", pallet = "classic")
+ct_missing <- ct[!ct %in% names(p1)]
+p_ambig <- c(Ambig="black", Ambig_01 = "grey15",Ambig_02 = "gray30")
+cell_colors = c(p1, p_ambig)
+
+# load("/dcl01/lieber/ajaffe/lab/deconvolution_bsp2/data/cell_colors.Rdata", verbose = TRUE)
+
+## Plot clusters in TSNE
+TSNE_HC_cellTypes <- ggcells(sce, mapping=aes(x=TSNE.1, y=TSNE.2, colour=cellType_hc)) +
+  geom_point(size = 0.2, alpha = 0.3) +
+  scale_color_manual(values = cell_colors, drop = TRUE) +
+  # my_theme +
+  coord_equal()
+
+ggsave(TSNE_HC_cellTypes + 
+         guides(colour = guide_legend(override.aes = list(size=2, alpha = 1))),
+       filename = here(plot_dir, "TSNE_HC-29_cellType.png"), width = 10)
+
+TSNE_km_cellTypes <- ggcells(sce, mapping=aes(x=TSNE.1, y=TSNE.2, colour=cellType_k)) +
+  geom_point(size = 0.2, alpha = 0.3) +
+  scale_color_manual(values = cell_colors, drop = TRUE) +
+  # scale_color_manual(values = cluster_colors) +
+  my_theme +
+  coord_equal()
+
+ggsave(TSNE_km_cellTypes + 
+         guides(colour = guide_legend(override.aes = list(size=2, alpha = 1))),
+       filename = here(plot_dir, "TSNE_mbkm-29_cellType.png"), width = 10)
+
+
+## markers
+my_plotMarkers(sce = sce, 
+               marker_list = markers.mathys.tran,
+               cat = "cellType_hc",
+               fill_colors = cell_colors,
+               pdf_fn = here(plot_dir, "HC_mathys_markers_ct.pdf"))
+
+my_plotMarkers(sce = sce, 
+               marker_list = markers.mathys.tran,
+               cat = "cellType_k",
+               fill_colors = cell_colors,
+               pdf_fn = here(plot_dir, "mb_kmeans_29_mathys_markers_ct.pdf"))
+
 
 #### Compare annotaitons ####
 library("pheatmap")
@@ -367,13 +441,7 @@ cluster_compare > 0
 hc_anno <- anno_hc %>% select(cluster, broad) %>% tibble::column_to_rownames("cluster")
 km_anno <- anno_k %>% select(cluster, broad) %>% tibble::column_to_rownames("cluster")
 
-ct <- unique(c(hc_anno$broad,km_anno$broad))
-p1 <- DeconvoBuddies::create_cell_colors(pallet = "classic")
-ct_missing <- ct[!ct %in% names(p1)]
-p2 <- DeconvoBuddies::create_cell_colors(ct_missing, pallet = "gg",split ="_")
-
-
-temp_pallet <- c(p1,p2)
+temp_pallet <- cell_colors[!grepl("_",names(cell_colors))] 
 
 png(here(plot_dir, "cluster_compare_heatmap.png"),height = 800, width = 800)
 pheatmap(cluster_compare2,
