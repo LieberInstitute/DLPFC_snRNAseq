adb6c8228b9fa293b29489b6537aa6392885e8c4
lahuuki <lahuuki@gmail.com>
2022-07-19 12:30:38 -0400

    Start to explore cell type proportions

---
+++ code/03_build_sce/10_cellType_prop.R
@@ -0,0 +1,132 @@
+library("SingleCellExperiment")
+library("DeconvoBuddies")
+library("tidyverse")
+library("here")
+
+#### plot setup  ####
+plot_dir = here("plots","03_build_sce","explore")
+load(here("processed-data", "03_build_sce","cell_type_colors.Rdata"), verbose = TRUE)
+
+cell_type_colors <- cell_type_colors[levels(sce$cellType_hc)]
+
+#### Load SCE ####
+load(here("processed-data","sce","sce_DLPFC.Rdata"), verbose = TRUE)
+
+pd <- as.data.frame(colData(sce))
+
+table(pd$cellType_broad_hc)
+# Astro Endo.Mural      Excit      Inhib      Micro      Oligo        OPC 
+# 3979       2157      24809      11067       1601      32051       1940
+
+n_nuc <- pd %>% 
+  group_by(Sample, subject, round, region, sex, age) %>% 
+  summarize(n_nuc = n()) %>%
+  ungroup()
+
+
+prop_all <- pd %>% 
+  count(Sample, cellType_hc) %>% 
+  left_join(n_nuc) %>%
+  mutate(prop = n/n_nuc)
+
+prop_broad <- pd %>% 
+  count(Sample, cellType_broad_hc) %>% 
+  left_join(n_nuc) %>%
+  mutate(prop = n/n_nuc)
+
+
+
+prop_boxplots <- prop_broad %>%
+  ggplot(aes(x = cellType_broad_hc, y  = prop, fill = cellType_broad_hc)) +
+  geom_boxplot(alpha = 0.4, outlier.shape = NA) + 
+  geom_jitter(width = 0.2, colour="black",pch=21) +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  # scale_color_manual(values = cell_type_colors_broad) +
+  theme_bw() +
+  theme(legend.position = "None")
+
+ggsave(prop_boxplots, filename = here(plot_dir, "prop_boxplots.png"))
+
+prop_boxplot_position <- prop_broad %>%
+  separate(Sample, into = c("BrNum","Position")) %>%
+  ggplot(aes(x = Position, y  = prop, fill = cellType_broad_hc)) +
+  geom_boxplot(alpha = 0.4, outlier.shape = NA) + 
+  geom_jitter(width = 0.2, colour="black",pch=21) +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  facet_wrap(~cellType_broad_hc, scales = "free_y") +
+  theme_bw() +
+  theme(legend.position = "None")
+
+ggsave(prop_boxplot_position, filename = here(plot_dir, "prop_boxplot_position.png"))
+
+
+#### Compositon Plots ####
+library(patchwork)
+## Mean Prop
+prop_bar_all <- plot_composition_bar(prop_all,
+                                       sample_col = "Sample",
+                                       ct_col = "cellType_hc",
+                                       min_prop_text = .02) +
+  scale_fill_manual(values = cell_type_colors) +
+  theme_bw() 
+
+ggsave(prop_bar_all, filename = here(plot_dir, "prop_bar_ALL.png"))
+
+broad_prop_bar_all <- plot_composition_bar(prop_broad,
+                                       sample_col = "Sample",
+                                       ct_col = "cellType_broad_hc",
+                                       min_prop_text = .02) +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  theme_bw() +
+  theme(legend.position = "None")
+
+ggsave(broad_prop_bar_all, filename = here(plot_dir, "prop_bar_broad_ALL.png"))
+
+ggsave(broad_prop_bar_all + theme(legend.position = "None") + labs(y = "Mean Proption - Broad") + prop_bar_all, 
+       filename = here(plot_dir, "prop_bar_fineVbroad_ALL.png"))
+
+## By Sample
+sample_prop_bar <- plot_composition_bar(prop_all,
+                                       sample_col = "Sample",
+                                       x_col = "Sample",
+                                       ct_col = "cellType_hc",
+                                       min_prop_text = .03) +
+  scale_fill_manual(values = cell_type_colors)  +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave(sample_prop_bar, filename = here(plot_dir, "prop_bar_Sample.png"), width = 12)
+
+broad_prop_bar <- plot_composition_bar(prop_broad,
+                                       sample_col = "Sample",
+                                       x_col = "Sample",
+                                       ct_col = "cellType_broad_hc",
+                                       min_prop_text = .02) +
+  scale_fill_manual(values = cell_type_colors_broad)+
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+ggsave(broad_prop_bar, filename = here(plot_dir, "prop_bar_broad_Sample.png"), width = 12)
+
+
+## Examine Round
+broad_prop_bar_round <- plot_composition_bar(prop_broad,
+                                       sample_col = "Sample",
+                                       x_col = "round",
+                                       ct_col = "cellType_broad_hc",
+                                       min_prop_text = .02) +
+  scale_fill_manual(values = cell_type_colors_broad) 
+
+ggsave(broad_prop_bar_round, filename = here(plot_dir, "prop_bar_broad_round.png"))
+
+## Position/region
+broad_prop_bar_pos <- plot_composition_bar(prop_broad,
+                                             sample_col = "Sample",
+                                             x_col = "region",
+                                             ct_col = "cellType_broad_hc",
+                                             min_prop_text = .02) +
+  scale_fill_manual(values = cell_type_colors_broad) +
+  theme_bw() +
+  labs(x = "Position")
+
+ggsave(broad_prop_bar_pos, filename = here(plot_dir, "prop_bar_broad_Position.png"))
