764b2ddef03a8fe0f42b3a114a5a3aaff64ea736
lahuuki <lahuuki@gmail.com>
2022-07-21 11:47:34 -0400

    Find markers for hc cell types, add sh file

--- code/03_build_sce/09_find_markers.R
+++ code/03_build_sce/09_find_markers.R
@@ -4,18 +4,29 @@ library("scater")
 library("here")
 library("sessioninfo")
 
-load(here("processed-data","sce","sce-DLPFC.Rdata"), verbose = TRUE)
+load(here("processed-data","sce","sce_DLPFC.Rdata"), verbose = TRUE)
 
 #### Find Markers ####
-(small_clusters <- cluster_tab[cluster_tab < 20])
-# mbk05 mbk18 mbk24 mbk29 
-# 11     3     2     7
+table(sce$cellType_hc)
+# Astro Endo.Mural_01 Endo.Mural_02      Excit_01      Excit_02      Excit_03      Excit_04      Excit_05 
+# 3979           446          1711          7927          2487          1309          2171          2532 
+# Excit_06      Excit_07      Excit_08      Excit_09      Excit_10      Excit_11      Excit_12      Excit_13 
+# 329           334          1463          2561          1079           482           420          1567 
+# Excit_14      Excit_15      Inhib_01      Inhib_02      Inhib_03      Inhib_04      Inhib_05      Inhib_06 
+# 82            66          5366          1267          1310           565          1192          1367 
+# Micro      Oligo_01      Oligo_02      Oligo_03           OPC 
+# 1601         23025          4732          4294          1940 
 
-sce <- sce[,!(sce$kmeans %in% names(small_clusters))]
-dim(sce)
-# [1] 36601 77581
-
-colLabels(sce) <- sce$kmeans
+colLabels(sce) <- sce$cellType_hc
 markers <- scran::findMarkers(sce, pval.type="all", direction="up")
 
-save(markers, file = here("processed-data", "03_build_sce","kmeans_29_markers.Rdata"))
+save(markers, file = here("processed-data", "03_build_sce","cell_type_markers.Rdata"))
+
+# sgejobs::job_single('09_find_markers', create_shell = TRUE, queue= 'bluejay', memory = '20G', command = "Rscript 09_find_markers.R")
+
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
