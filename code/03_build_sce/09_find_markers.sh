2e859a28e11092c9c40ebe6d69ee3fd91e3102b1
lahuuki <lahuuki@gmail.com>
2022-08-04 17:15:59 -0400

    Comment out GPU code

--- code/03_build_sce/09_find_markers.sh
+++ code/03_build_sce/09_find_markers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=50G,h_vmem=50G,h_fsize=100G
+#$ -l mem_free=150G,h_vmem=150G,h_fsize=100G
 #$ -N find_markers
 #$ -o logs/09_find_markers.txt
 #$ -e logs/09_find_markers.txt
@@ -21,6 +21,19 @@ module load conda_R
 
 ## List current modules for reproducibility
 module list
+
+## Check for GPUs
+# NUM_GPUS=1
+# avail_gpus=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | cut -d " " -f 1 | awk -v usage="$USAGE_CUTOFF" '$1 < usage {print NR - 1}')
+
+#  Simply exit with an error if there are no GPUs left
+#if [[ -z $avail_gpus ]]; then
+#    echo "No GPUs are available."
+#    exit 1
+#fi
+
+#export CUDA_VISIBLE_DEVICES=$(echo "$avail_gpus" | head -n $NUM_GPUS | paste -sd ",")
+
 
 ## Edit with your job command
 Rscript 09_find_markers.R
