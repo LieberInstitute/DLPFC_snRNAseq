a90a4354281099d124fd7fe2c4de850f35088d7e
lahuuki <lahuuki@gmail.com>
2022-08-16 16:54:42 -0400

    Add notes and questions about code #12

--- code/05_explore_sce/azimuth_codeR.R
+++ code/05_explore_sce/azimuth_codeR.R
@@ -6,9 +6,17 @@ library(Seurat)
 library(Azimuth)
 library(SeuratData)
 library(patchwork)
+library(here)
 
 # transform reference dataset to a Seurat object compatible with Azimuth####
+## What refrence should we use?
+# reference = "humancortexref"
+# reference <- LoadReference(path = "https://seurat.nygenome.org/azimuth/references/v1.0.0/human_motorcortex")
+# Can we use Azimuth style refrence? or do we need to create a new one?
+
 reference.so <- CreateSeuratObject(counts = counts(reference), meta.data = data.frame(colData(reference))) # where reference is a SingleCellExperiment object
+
+## Normalize data
 reference.so <- SCTransform(reference.so,assay = "RNA", new.assay.name = "SCT", variable.features.n = 2000,
                             verbose = TRUE,conserve.memory=TRUE) # conserve.memory=TRUE for large dataset (no R crash)
 
@@ -20,30 +28,41 @@ reference.so <- RunUMAP(reference.so, assay = "SCT", reduction = "PCA", dims = s
 reference.so$subclass <- as.factor(reference.so$subclass) 
 Idents(object = reference.so) <- "subclass"
 
+
 reference.azimuth <- AzimuthReference(reference.so, refUMAP = "umap",refDR = "PCA",refAssay = "SCT",dims = 1:50,
                                       metadata = c("subclass"), verbose = TRUE) # object compatible with Azimuth
 
 # save reference
-ref.dir <- "reference/"
+ref.dir <- here("processed-data","05_explore_sce","05_azimuth_validataion","reference")
+
 SaveAnnoyIndex(object = reference.azimuth[["refdr.annoy.neighbors"]], file = file.path(ref.dir, "idx.annoy"))
 saveRDS(object = reference.azimuth, file = file.path(ref.dir, "ref.Rds"))
 
 ## Cell Annotation with Azimuth for each sample ####
 # create a Seurat object for each sample from SingleCellExperiment object
+
+## should we also loop by Sample?
 query <- lapply(singlet.sce, function(sce)  CreateSeuratObject(counts = counts(sce), 
                                                                meta.data = data.frame(colData(sce)),
                                                                project = "SD"))
-query <- lapply(query, function(so) RunAzimuth(so, reference = "reference/")) ## Cell annotation with Azimuth
+
+query <- lapply(query, function(so) RunAzimuth(so, reference = ref.dir)) ## Cell annotation with Azimuth
 # In predicted.subclass there are labels obtained from the annotation
+
+## What happens past this point? Is it relevant for cell type annotation?
+
 ## Integration####
+## why not SCTransform? 
 query <- lapply(query, NormalizeData, verbose = TRUE) # normalization for each sample
 query <- lapply(query, FindVariableFeatures, nfeatures = 2e3,
                 selection.method = "vst", verbose = TRUE) # find most variable features for each sample
 query <- lapply(query, ScaleData, verbose = TRUE) # scale the data, for each sample
+
 # find anchors & integrate
 as <- FindIntegrationAnchors(query, verbose = FALSE)
 seurat.obj.integrated <- IntegrateData(anchorset = as, dims = seq_len(30), verbose = FALSE) # Seurat object
 DefaultAssay(seurat.obj.integrated) <- "integrated"
+
 # scale integrated data
 seurat.obj.integrated <- ScaleData(seurat.obj.integrated, verbose = FALSE) # rescaled 
 seurat.obj.integrated <- RunPCA(seurat.obj.integrated, assay = "integrated", npcs = 30, verbose = FALSE,
