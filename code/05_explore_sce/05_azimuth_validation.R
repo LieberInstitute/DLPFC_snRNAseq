ad8e5481abbcc3548991b579224ac4027e18a25d
lahuuki <lahuuki@gmail.com>
2022-08-16 13:32:40 -0400

    Move refrence prep to other script, plot heatmaps of basic azimuth run #12

--- code/05_explore_sce/05_azimuth_validation.R
+++ code/05_explore_sce/05_azimuth_validation.R
@@ -11,65 +11,20 @@ library("here")
 library("pheatmap")
 
 
-#### Plotting ####
+#### Plot Setup ####
 plot_dir = here("plots","05_explore_sce","05_azimuth_validation")
 if(!dir.exists(plot_dir)) dir.create(plot_dir)
 
 
-## Use Neuron DLPFC as refrence?
-# load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/revision/regionSpecific_DLPFC-n3_cleaned-combined_SCE_LAH2021.rda", verbose = TRUE)
-
-# transform reference dataset to a Seurat object compatible with Azimuth####
-
-## Only use top 2k deviant genes
-# hdgs.hb <- rownames(sce)[order(rowData(sce)$binomial_deviance, decreasing=T)][1:2000]
-
-reference.so <- CreateSeuratObject(counts = as.matrix(counts(sce.dlpfc)), meta.data = data.frame(colData(sce.dlpfc))) # where reference is a SingleCellExperiment object
-# reference.so <- CreateSeuratObject(counts = as.matrix(counts(sce[hdgs.hb,])), meta.data = data.frame(colData(sce))) # where reference is a SingleCellExperiment object
-# reference.so <- CreateSeuratObject(counts = counts(reference), meta.data = data.frame(colData(reference))) # where reference is a SingleCellExperiment object
-
-reference.so <- SCTransform(reference.so,assay = "RNA", new.assay.name = "SCT", variable.features.n = 2000,
-                            verbose = TRUE,conserve.memory=TRUE) # conserve.memory=TRUE for large dataset (no R crash)
-
-reference.so <- RunPCA(reference.so, assay = "SCT", npcs = 50, verbose = FALSE,
-                       reduction.name = "PCA",return.model=TRUE) # npcs must be 50
-
-reference.so <- RunUMAP(reference.so, assay = "SCT", reduction = "PCA", dims = seq_len(50),
-                        seed.use = 1, verbose = FALSE, reduction.name = "umap",return.model=TRUE) # reduction.name = "umap" to be able to create the object of interest
-
-reference.so$subclass <- as.factor(reference.so$cellType) 
-Idents(object = reference.so) <- "subclass"
-
-reference.azimuth <- AzimuthReference(reference.so, refUMAP = "umap",refDR = "PCA",refAssay = "SCT",dims = 1:50,
-                                      metadata = c("subclass"), verbose = TRUE) # object compatible with Azimuth
-
-# save reference
-ref.dir <- here("processed-data","05_explore_sce","05_azimuth_validation","reference")
-SaveAnnoyIndex(object = reference.azimuth[["refdr.annoy.neighbors"]], file = file.path(ref.dir, "idx.annoy"))
-saveRDS(object = reference.azimuth, file = file.path(ref.dir, "ref.Rds"))
-
-
-## Cell Annotation with Azimuth for each sample ####
+## Cell Annotation with RunAzimuth####
 
 ## Use Azimuth Refrence ##
 ## new data
 load(here("processed-data","sce","sce_DLPFC.Rdata"), verbose = TRUE)
 
-# create a Seurat object for each sample from SingleCellExperiment object
-# query <- lapply(singlet.sce, function(sce)  CreateSeuratObject(counts = counts(sce), 
-#                                                                meta.data = data.frame(colData(sce)),
-#                                                                project = "SD"))
-
 query <- CreateSeuratObject(counts = as.matrix(counts(sce)), 
                             meta.data = data.frame(colData(sce)),
                             project = "DLPFC")
-
-rm(sce)
-
-## Save 
-# SeuratDisk::SaveH5Seurat(query, filename = here("processed-data","05_explore_sce","05_azimuth_validation","sce_DLPFC.h5Seurat"))
-
-# query <- RunAzimuth(query, reference = ref.dir) ## Cell annotation with Azimuth
 
 query <- RunAzimuth(query, reference = "humancortexref") ## Cell annotation with Azimuth
 
@@ -79,25 +34,53 @@ table(query$predicted.subclass)
 # Lamp5  Micro-PVM      Oligo        OPC      Pvalb       Sncg        Sst  Sst Chodl        Vip       VLMC 
 # 802       2507      11384       1836       1513        243       4689         87       4178       1544 
 
+## Save 
+# SeuratDisk::SaveH5Seurat(query, filename = here("processed-data","05_explore_sce","05_azimuth_validation","sce_DLPFC.h5Seurat"))
+
+#### Compare to HC annotations 
 ct_annos <- table(query$cellType_hc, query$predicted.subclass)
 
+png(here(plot_dir, "azimuth_v_hc.png"),height = 800, width = 800)
+pheatmap(ct_annos,
+         main = "Number of Nuclei",
+         cluster_rows = FALSE,
+         cluster_cols = FALSE)
+dev.off()
 
+## Prop of HC annotation groups
 ct_annos_prop <- sweep(ct_annos,1,rowSums(ct_annos),`/`)
-rowSums(ct_annos_prop)
 
 ct_max <- apply(ct_annos_prop, 1, max)
 table(ct_max  > 0.7)
 
 sum(ct_annos_prop > .90)
 
-png(here(plot_dir, "azimuth_v_hc.png"),height = 800, width = 800)
-pheatmap(ct_annos_prop
-         # ,
-         # annotation_col= hc_anno,
-         # annotation_row = km_anno,
-         # annotation_colors = list(broad = temp_pallet)
-)
+png(here(plot_dir, "azimuth_v_hc-prop.png"),height = 800, width = 800)
+pheatmap(ct_annos_prop,
+         main = "Proportion of HC Cluster",
+         cluster_rows = FALSE,
+         cluster_cols = FALSE)
 dev.off()
+
+png(here(plot_dir, "azimuth_v_hc-prop-cluster.png"),height = 800, width = 800)
+pheatmap(ct_annos_prop,
+         main = "Number of Nuclei")
+dev.off()
+
+library(bluster)
+jacc.mat <- linkClustersMatrix(query$cellType_hc, query$predicted.subclass)
+
+png(here(plot_dir, "azimuth_v_hc-jacc.png"),height = 800, width = 800)
+pheatmap(jacc.mat,
+         main = "Strength of the correspondence between Azimuth & HC")
+dev.off()
+
+pairwiseRand(query$cellType_hc, query$predicted.subclass, mode="index")
+# [1] 0.3094603
+pairwiseRand(query$cellType_broad_hc, query$predicted.subclass, mode="index")
+# [1] 0.2558036
+pairwiseRand(query$cellType_k, query$predicted.subclass, mode="index")
+# [1] 0.2441774
 
 library(ggplot2)
 p1 <- DimPlot(query, group.by = "predicted.subclass", label = TRUE, label.size = 3, repel = TRUE) +
