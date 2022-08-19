f6c0087c67f3da0fcaab44b1a019d0cb97f46e0a
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-08-19 11:42:16 -0400

    Running into this error:
    Error in value[[3L]](cond) :
      'assay(<SingleCellExperiment>, i="character", ...)' invalid subscript 'i'
    H5Fis_hdf5() returned an error
    
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

--- code/06_iSEE_apps/app.R
+++ code/06_iSEE_apps/app.R
@@ -3,42 +3,43 @@ library("iSEE")
 library("shiny")
 library("paletteer")
 
-load("sce_for_iSEE_LS.rda", verbose = TRUE)
+load("sce_DLPFC.Rdata", verbose = TRUE)
 
-stopifnot(all(unique(sce.ls.small$cellType.final) %in% names(cell_cols.clean)))
+cell_type_colors <- metadata(sce)$cell_type_colors[levels(sce$cellType_hc)]
+stopifnot(all(unique(sce$cellType_hc) %in% names(cell_type_colors)))
 
 ## Don't run this on app.R since we don't want to run this every single time
-# lobstr::obj_size(sce.ls.small)
+# lobstr::obj_size(sce)
 # 876.33 MB
 
 source("initial.R", print.eval = TRUE)
 
 ## From https://github.com/LieberInstitute/10xPilot_snRNAseq-human/blob/810b47364af4c8afe426bd2a6b559bd6a9f1cc98/shiny_apps/tran2021_AMY/app.R#L10-L14
 ## Related to https://github.com/iSEE/iSEE/issues/568
-colData(sce.ls.small) <- cbind(
-  colData(sce.ls.small)[, !colnames(colData(sce.ls.small)) %in% c("Sample", "cellType.final")],
-  colData(sce.ls.small)[, c("cellType.final", "Sample")]
+colData(sce) <- cbind(
+  colData(sce)[, !colnames(colData(sce)) %in% c("Sample", "cellType_hc")],
+  colData(sce)[, c("cellType_hc", "Sample")]
 )
 
-sce.ls.small$Sample <- as.factor(sce.ls.small$Sample)
+sce$Sample <- as.factor(sce$Sample)
 
-sce.ls.small <- registerAppOptions(sce.ls.small, color.maxlevels = length(cell_cols.clean))
+sce <- registerAppOptions(sce, color.maxlevels = length(cell_type_colors))
 iSEE(
-    sce.ls.small,
-    appTitle = "mm_LS_2022",
-    initial = initial,
-    colormap = ExperimentColorMap(colData = list(
-        Sample = function(n) {
-            cols <- paletteer::paletteer_d(
-                palette = "RColorBrewer::Dark2",
-                n = length(unique(sce.ls.small$Sample))
-            )
-            cols <- as.vector(cols)
-            names(cols) <- levels(sce.ls.small$Sample)
-            return(cols)
-        },
-        cellType.final = function(n) {
-            return(cell_cols.clean)
-        }
-    ))
+    sce,
+    appTitle = "DLPFC_2022"#,
+    #initial = initial,
+    # colormap = ExperimentColorMap(colData = list(
+    #     Sample = function(n) {
+    #         cols <- paletteer::paletteer_d(
+    #             palette = "RColorBrewer::Dark2",
+    #             n = length(unique(sce$Sample))
+    #         )
+    #         cols <- as.vector(cols)
+    #         names(cols) <- levels(sce$Sample)
+    #         return(cols)
+    #     },
+        # cellType_hc = function(n) {
+        #     return(cell_type_colors)
+        # }
+    #))
 )
