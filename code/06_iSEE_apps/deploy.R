a4bc8fb16bfd319216b562e1db053a3ca16111b3
Heena Divecha <heenadivecha@eb-li-612uml85.win.ad.jhu.edu>
2022-08-23 13:38:46 -0400

    Added 'options(rsconnect.max.bundle.size= 1024^3 * 4.9)' to fix the size issue described in the previous commit message
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>
    Co-authored-by: Louise Huuki <lahuuki@gmail.com>

--- code/06_iSEE_apps/deploy.R
+++ code/06_iSEE_apps/deploy.R
@@ -3,6 +3,7 @@ library("rsconnect")
 source("token.R")
 
 options(repos = BiocManager::repositories())
+options(rsconnect.max.bundle.size= 1024^3 * 4.9)
 rsconnect::deployApp(
     appFiles = c("app.R", "sce_DLPFC_annotated/", "initial.R"),
     appName = "DLPFC_snRNA-seq_2022",
