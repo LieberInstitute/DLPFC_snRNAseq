f9cad934e9f01fd09de47c83cacfd339447679f4
lahuuki <lahuuki@gmail.com>
2022-08-25 16:25:23 -0400

    Move computeEnrichment to seperate script, calc cor with top 100

--- code/05_explore_sce/07_spatial_registration.R
+++ code/05_explore_sce/07_spatial_registration.R
@@ -13,34 +13,21 @@ library("sessioninfo")
 source("utils.R")
 
 # load sc data
-# load(file = "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata")
-sce <- loadHDF5SummarizedExperiment(here("processed-data","sce","sce_DLPFC_annotated"))
+load(here("processed-data","sce","sce_DLPFC.Rdata"), verbose = TRUE)
 
-# var_oi <- "cellType_hc"
-# covars <- c("Position", "age", "sex")
+## previously saved data
+load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/12_spatial_registration_sc/results_specificity.RDS", verbose = TRUE)
+# results_specificity
+head(results_specificity[,grep("Excit_07",colnames(results_specificity))])
+#                 p_value_Excit_07 fdr_Excit_07 # No T-stats for Excit_07 ???
+# ENSG00000000419        0.8128746    0.9210253
+# ENSG00000001084        0.1046406    0.3697436
+# ENSG00000001461        0.1951751    0.4999134
+# ENSG00000001629        0.4219413    0.7080032
+# ENSG00000001630        0.9801410    0.9959250
+# ENSG00000001631        0.4906153    0.7499232
 
-rownames(sce) <- rowData(sce)$gene_id # have to make row names of object the ensembl id instead of gene names
-colData(sce)$Position <- as.factor(colData(sce)$Position)
-colData(sce)$age <- as.numeric(colData(sce)$age)
-colData(sce)$sex <- as.factor(colData(sce)$sex)
-colnames(colData(sce))[1] <- "sample_id"
-
-colData(sce)
-
-spe_pseudo <- scuttle::aggregateAcrossCells(
-  sce,
-  DataFrame(
-    BayesSpace = colData(sce)[["cellType_hc"]],
-    sample_id = sce$sample_id
-  )
-)
-
-results_specificity <- computeEnrichment(sce, var_oi = "cellType_hc", covars = c("Position", "age", "sex"))
-
-# save(results_specificity, file = "/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/12_spatial_registration_sc/results_specificity.RDS")
-
-load("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/processed-data/rdata/spe/12_spatial_registration_sc/results_specificity.RDS")
-
+## Extract t-stats
 specificity_stats <- results_specificity[, grep("^t_stat", colnames(results_specificity))]
 colnames(specificity_stats) <- gsub("^t_stat_", "", colnames(specificity_stats))
 
@@ -53,21 +40,27 @@ colnames(specificity_stats) # missing Excit_07
 # [15] "Excit_13"      "Excit_14"      "Excit_15"      "Inhib_01"      "Inhib_02"      "Inhib_03"      "Inhib_04"     
 # [22] "Inhib_05"      "Inhib_06"      "Micro"         "Oligo_01"      "Oligo_02"      "Oligo_03"      "OPC"        
 
-# vs manual annotations
+#### cell types vs manual annotations ####
 modeling_results <- fetch_data(type = "modeling_results")
-cor <- layer_stat_cor(
+
+## HC anno vs. top 100
+cor_hc_top100 <- layer_stat_cor(
   specificity_stats,
   modeling_results,
   model_type = names(modeling_results)[2],
   reverse = FALSE,
-  top_n = NULL
+  top_n = 100
 )
 
-cor
-save(cor, file = here("processed-data","05_explore_sce","07_sptatial_registration","layer_cor.Rdata"))
+save(cor_hc_top100 , file = here("processed-data","05_explore_sce","07_sptatial_registration","layer_cor.Rdata"))
 
-pdf("/dcs04/lieber/lcolladotor/spatialDLPFC_LIBD4035/spatialDLPFC/plots/12_spatial_registration_sc/spatial_registration_plot_sc_v_manual.pdf")
-layer_stat_cor_plot(cor, max = 0.7)
+## plot dir
+plot_dir <- here("plots", "05_explore_sce",  "07_spatial_registration")
+if(!dir.exists(plot_dir)) dir.create(plot_dir)
+
+## plot 
+pdf(here(plot_dir,"spatial_registration_plot_hc_v_manual_top100.pdf"))
+layer_stat_cor_plot(cor_hc_top100, max = max(cor_hc_top100))
 dev.off()
 
 ## Azimuth registration ##
