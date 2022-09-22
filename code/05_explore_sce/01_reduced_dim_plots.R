229fda7aeef9203daf730f7d1f2bbb7b279c84ea
lahuuki <lahuuki@gmail.com>
2022-09-22 12:25:18 -0400

    Fix theme to theme_bw + no grid, update plots

--- code/05_explore_sce/01_reduced_dim_plots.R
+++ code/05_explore_sce/01_reduced_dim_plots.R
@@ -1,14 +1,21 @@
 library("SingleCellExperiment")
 library("scater")
 library("jaffelab")
+library("patchwork")
 library("here")
 
 #### Load Data ####
 load(here("processed-data", "sce", "sce_DLPFC.Rdata"), verbose = TRUE)
 
 #### Set Up Plotting ####
-my_theme <- theme_classic() +
-    theme(text = element_text(size = 15))
+# my_theme <- theme_classic() +
+#     theme(text = element_text(size = 15),
+#           strip.background = element_rect(colour = "black", fill = "light grey"))
+
+my_theme <- theme_bw() +
+  theme(text = element_text(size = 15),
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
 
 plot_dir <- here("plots", "05_explore_sce", "01_reduced_dim_plots")
 if (!dir.exists(plot_dir)) dir.create(plot_dir)
@@ -36,40 +43,50 @@ filename = here(plot_dir, "UMAP_cellType.pdf"), width = 9
 )
 
 ## No legends
-ggsave(UMAP_cellTypes_hc +
-    theme(legend.position = "None"),
+UMAP_cellTypes_hc_no_legend <- UMAP_cellTypes_hc + theme(legend.position = "None")
+
+ggsave(UMAP_cellTypes_hc_no_legend,
 filename = here(plot_dir, "UMAP_cellType_no_legend.png")
 )
-ggsave(UMAP_cellTypes_hc +
-    theme(legend.position = "None"),
+ggsave(UMAP_cellTypes_hc_no_legend,
 filename = here(plot_dir, "UMAP_cellType_no_legend.pdf")
 )
 
 ## Add facet for cell types
-ggsave(UMAP_cellTypes_hc +
-    facet_wrap(~cellType_hc) +
-      scale_x_continuous(n.breaks = 3) +
-      scale_y_continuous(n.breaks = 3) +
-    theme(legend.position = "None"),
+UMAP_cellTypes_hc_facet <- UMAP_cellTypes_hc_no_legend +
+  facet_wrap(~cellType_hc) +
+  scale_x_continuous(n.breaks = 3) +
+  scale_y_continuous(n.breaks = 3)
+
+ggsave(UMAP_cellTypes_hc_facet,
 filename = here(plot_dir, "UMAP_cellType_facet.png"), 
 )
-ggsave(UMAP_cellTypes_hc +
-         facet_wrap(~cellType_hc) +
-         scale_x_continuous(n.breaks = 3) +
-         scale_y_continuous(n.breaks = 3) +
-         theme(legend.position = "None"),
+ggsave(UMAP_cellTypes_hc_facet,
        filename = here(plot_dir, "UMAP_cellType_facet.pdf"), 
 )
 
+## Add full + facet for cell types
+ggsave(UMAP_cellTypes_hc_no_legend +
+         UMAP_cellTypes_hc_facet +
+         theme(axis.title.y = element_blank()),
+       filename = here(plot_dir, "UMAP_cellType_full_facet.png"), 
+       width = 13
+)
+
+ggsave(UMAP_cellTypes_hc_no_legend +
+         UMAP_cellTypes_hc_facet +
+         theme(axis.title.y = element_blank()),
+       filename = here(plot_dir, "UMAP_cellType_full_facet.pdf"), 
+       width = 13
+)
 
 #### Plot clusters in TSNE ####
-## Cell Type UMAP
 TSNE_cellTypes_hc <- ggcells(sce, mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType_hc)) +
   geom_point(size = 0.2, alpha = 0.3) +
   scale_color_manual(values = cell_type_colors) +
   my_theme +
   coord_equal() +
-  labs(x = "tSNE Dimension 1", y = "tSNE Dimension 2")
+  labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
 
 ## Add large legends
 ggsave(TSNE_cellTypes_hc +
@@ -82,95 +99,78 @@ ggsave(TSNE_cellTypes_hc +
 )
 
 ## No legends
-ggsave(TSNE_cellTypes_hc +
-         theme(legend.position = "None"),
+TSNE_cellTypes_hc_no_legend <- TSNE_cellTypes_hc + theme(legend.position = "None")
+
+ggsave(TSNE_cellTypes_hc_no_legend,
        filename = here(plot_dir, "TSNE_cellType_no_legend.png")
 )
-ggsave(TSNE_cellTypes_hc +
-         theme(legend.position = "None"),
+ggsave(TSNE_cellTypes_hc_no_legend,
        filename = here(plot_dir, "TSNE_cellType_no_legend.pdf")
 )
 
 ## Add facet for cell types
-ggsave(TSNE_cellTypes_hc +
-         facet_wrap(~cellType_hc) +
-         # scale_x_continuous(n.breaks = 3) +
-         # scale_y_continuous(n.breaks = 3) +
-         theme(legend.position = "None"),
+TSNE_cellTypes_hc_facet <- TSNE_cellTypes_hc_no_legend +
+  facet_wrap(~cellType_hc) 
+
+ggsave(TSNE_cellTypes_hc_facet,
        filename = here(plot_dir, "TSNE_cellType_facet.png"), 
 )
-ggsave(TSNE_cellTypes_hc +
-         facet_wrap(~cellType_hc) +
-         # scale_x_continuous(n.breaks = 3) +
-         # scale_y_continuous(n.breaks = 3) +
-         theme(legend.position = "None"),
+ggsave(TSNE_cellTypes_hc_facet,
        filename = here(plot_dir, "TSNE_cellType_facet.pdf"), 
 )
 
+## Add full + facet for cell types
+ggsave(TSNE_cellTypes_hc_no_legend +
+         TSNE_cellTypes_hc_facet +
+         theme(axis.title.y = element_blank()),
+       filename = here(plot_dir, "TSNE_cellType_full_facet.png"), 
+       width = 13
+)
 
-#### layer annotaions ####
-UMAP_cellType_layer <- ggcells(sce[, !is.na(sce$cellType_layer)], mapping = aes(x = UMAP.1, y = UMAP.2, colour = cellType_layer)) +
+ggsave(TSNE_cellTypes_hc_no_legend +
+         TSNE_cellTypes_hc_facet +
+         theme(axis.title.y = element_blank()),
+       filename = here(plot_dir, "TSNE_cellType_full_facet.pdf"), 
+       width = 13
+)
+
+#### TSNE layer annotations ####
+TSNE_cellType_layer <- ggcells(sce[, !is.na(sce$cellType_layer)], mapping = aes(x = TSNE.1, y = TSNE.2, colour = cellType_layer)) +
   geom_point(size = 0.2, alpha = 0.3) +
   scale_color_manual(values = cell_type_colors_layer) +
   my_theme +
-  coord_equal()
+  coord_equal()+
+  labs(x = "TSNE Dimension 1", y = "TSNE Dimension 2")
 
-ggsave(UMAP_cellType_layer +
+ggsave(TSNE_cellType_layer +
          guides(colour = guide_legend(override.aes = list(size = 2, alpha = 1))),
-       filename = here(plot_dir, "UMAP_cellType_layer.png"), width = 9
+       filename = here(plot_dir, "TSNE_cellType_layer.png"), width = 9
 )
 
-ggsave(UMAP_cellType_layer +
+ggsave(TSNE_cellType_layer +
+         guides(colour = guide_legend(override.aes = list(size = 2, alpha = 1))),
+       filename = here(plot_dir, "TSNE_cellType_layer.pdf"), width = 9
+)
+
+## no legend
+ggsave(TSNE_cellType_layer +
          theme(legend.position = "None"),
-       filename = here(plot_dir, "UMAP_cellType_layer_no_legend.png")
+       filename = here(plot_dir, "TSNE_cellType_layer_no_legend.png")
+)
+ggsave(TSNE_cellType_layer +
+         theme(legend.position = "None"),
+       filename = here(plot_dir, "TSNE_cellType_layer_no_legend.pdf")
 )
 
-#### Explore UMI ####
-UMI_ct_k <- ggcells(sce, mapping = aes(x = cellType_k, y = sum, fill = cellType_k)) +
-    geom_boxplot() +
-    scale_fill_manual(values = cell_type_colors[levels(sce$cellType_k)], drop = TRUE) +
-    my_theme +
-    theme(
-        legend.position = "None", axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1)
-    )
-
-ggsave(UMI_ct_k, filename = here(plot_dir, "UMI_mbkm-29_cellType.png"), width = 10)
 
 
-UMI_ct_hc <- ggcells(sce, mapping = aes(x = cellType_hc, y = sum, fill = cellType_hc)) +
-    geom_boxplot() +
-    scale_fill_manual(values = cell_type_colors) +
-    my_theme +
-    theme(
-        legend.position = "None", axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1)
-    )
+# sgejobs::job_single('01_reduce_dim_plots', create_shell = TRUE, queue= 'bluejay', memory = '10G', command = "Rscript 01_reduce_dim_plots.R")
 
-ggsave(UMI_ct_hc, filename = here(plot_dir, "UMI_HC_cellType.png"), width = 10)
+## Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
 
-
-#### Explore doublet scores
-dbs_ct_k <- ggcells(sce, mapping = aes(x = cellType_k, y = doubletScore, fill = cellType_k)) +
-    geom_boxplot() +
-    scale_fill_manual(values = cell_type_colors) +
-    my_theme +
-    theme(
-        legend.position = "None", axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1)
-    )
-
-ggsave(dbs_ct_k, filename = here(plot_dir, "doubletScore_mbkm-29_cellType.png"), width = 10)
-
-
-dbs_ct_hc <- ggcells(sce, mapping = aes(x = cellType_hc, y = doubletScore, fill = cellType_hc)) +
-    geom_boxplot() +
-    scale_fill_manual(values = cell_type_colors) +
-    my_theme +
-    theme(
-        legend.position = "None", axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1)
-    )
-
-ggsave(dbs_ct_hc, filename = here(plot_dir, "doubletScore_HC_cellType.png"), width = 10)
 
