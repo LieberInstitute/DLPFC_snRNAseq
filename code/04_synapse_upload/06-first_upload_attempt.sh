cac823c2292a3e8a468f5e5e6758aa95b610cc77
Nick-Eagles <nick.eagles@libd.org>
2022-09-26 11:09:27 -0400

    Small fixes after interacting testing

--- code/04_synapse_upload/06-first_upload_attempt.sh
+++ code/04_synapse_upload/06-first_upload_attempt.sh
@@ -6,18 +6,20 @@
 #  Get absolute path to 'DLPFC_snRNAseq' repo
 base_dir=$(git rev-parse --show-toplevel)
 
-screen -S "upload_DLPFC_snRNAseq"
-
 module load synapse/2.6.0
 
 #   Perform a dry run as an initial test
-python 06-first_upload_attempt.py \
+python $base_dir/code/04_synapse_upload/06-first_upload_attempt.py \
     -c ~/synapse_credentials.yaml \
     -m "$base_dir/processed-data/04_synapse_upload/manifest.tsv" \
     -d
 
+screen -S "upload_DLPFC_snRNAseq"
+base_dir=$(git rev-parse --show-toplevel)
+module load synapse/2.6.0
+
 #   Perform the actual upload
-python 06-first_upload_attempt.py \
+python $base_dir/code/04_synapse_upload/06-first_upload_attempt.py \
     -c ~/synapse_credentials.yaml \
-    -m "$base_dir/processed-data/04_synapse_upload/manifest.tsv" >
-    $base_dir/processed-data/04_synapse_upload/06-first_upload_attempt.log 2>&1
+    -m "$base_dir/processed-data/04_synapse_upload/manifest.tsv" \
+    > $base_dir/processed-data/04_synapse_upload/06-first_upload_attempt.log 2>&1
