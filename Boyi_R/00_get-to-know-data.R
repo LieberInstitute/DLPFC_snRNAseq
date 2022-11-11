1348a3a5fa8d55ac277f6c760460e714859e8f56
Boyi Guo <boyi.guo.work@gmail.com>
2022-11-11 16:36:16 -0500

    Failed to install scater on RStudio Server

--- Boyi_R/00_get-to-know-data.R
+++ Boyi_R/00_get-to-know-data.R
@@ -3,8 +3,8 @@ library(SingleCellExperiment)
 
 
 # Load Batch Effect Correct Data
-# load(file = "processed-data/03_build_sce/sce_harmony_Sample.Rdata")
-sce <- readRDS(file = "processed-data/03_build_sce/DLPFC_sce.rds")
+load(file = "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/03_build_sce/sce_harmony_Sample.Rdata")
+# sce <- readRDS(file = "processed-data/03_build_sce/DLPFC_sce.rds")
 col_data <- colData(sce) |> as.data.frame()
 
 
@@ -13,6 +13,10 @@ col_data |> group_by(subject) |> summarize(n = n_distinct(region))
 
 
 # Sample Br2720
+# Why when I do this subsetting, we have loading all these packages again?
+# Which object is doing this annoying thing!
 sce_Br2720 <- sce[, sce$subject=="Br2720"]
 
+
+library(RSoptSC)
 
